c8d42196cbfb753affa0bd4fc2fd355211f7e70e
Kendell R <KTibow@users.noreply.github.com>
2020-07-12 11:40:22 -0700

    Update and rename ota-example/ota-example.ino to ota_example/ota_example.ino

---
+++ ota_example/ota_example.ino
@@ -0,0 +1,52 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+#include <WebServer.h>
+
+WebServer server(80);
+const char* host = "esp32";
+bool ledOn = false;
+const char* ssid = "yourNetworkName";
+const char* password = "yourNetworkPassword";
+
+void handleBlinks(void * pvParameters) {
+  while(true) {
+    ledOn = true;
+    delay(1000);
+    ledOn = false;
+    delay(1000);
+  }
+}
+
+void setup() {
+  WiFi.disconnect(true);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoConnect(true);
+  WiFi.begin(ssid, password);
+  delay(100);
+  byte count = 0;
+  while(WiFi.status() != WL_CONNECTED && count < 20) {
+    count++;
+    delay(500);
+  }
+  while(WiFi.status() != WL_CONNECTED);
+  delay(100);
+  #include "otaroute.h"
+  pinMode(LED_BUILTIN, OUTPUT);
+  xTaskCreatePinnedToCore(
+    handleBlinks,
+    "handleBlinks",
+    10000,
+    NULL,
+    1,
+    &Task,
+    (xPortGetCoreID() == 1) ? 0 : 1);
+}
+void loop() {
+  int tt = millis();
+  digitalWrite(LED_BUILTIN, ledOn);
+  while (millis() - tt < 10) {
+    server.handleClient();
+  }
+}
